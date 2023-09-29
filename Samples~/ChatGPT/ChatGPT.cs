6af1073ecbc279ff1d388c7dad786c3df01b4413
plix <palle.torsson@gmail.com>
2023-09-29 11:43:55 +0200

    Update ChatGPT.cs adding comment in swedish

--- Samples~/ChatGPT/ChatGPT.cs
+++ Samples~/ChatGPT/ChatGPT.cs
@@ -4,77 +4,81 @@ using System.Collections.Generic;
 
 namespace OpenAI
 {
+    // Klassen ChatGPT är en MonoBehaviour, vilket betyder att den kan fästas på ett Unity-objekt.
     public class ChatGPT : MonoBehaviour
     {
-        [SerializeField] private InputField inputField;
-        [SerializeField] private Button button;
-        [SerializeField] private ScrollRect scroll;
+        // Fält för att koppla UI-element i Unity-redigeraren.
+        [SerializeField] private InputField inputField;  // Inmatningsfältet för användaren att skriva meddelanden.
+        [SerializeField] private Button button;  // Knappen för att skicka meddelanden.
+        [SerializeField] private ScrollRect scroll;  // Skrollområdet där meddelanden visas.
         
-        [SerializeField] private RectTransform sent;
-        [SerializeField] private RectTransform received;
+        // Fält för meddelandevisningslayout.
+        [SerializeField] private RectTransform sent;  // Layout för skickade meddelanden.
+        [SerializeField] private RectTransform received;  // Layout för mottagna meddelanden.
 
-        private float height;
-        private OpenAIApi openai = new OpenAIApi();
+        private float height;  // Variabel som lagrar höjden på chattfönstret.
+        private OpenAIApi openai = new OpenAIApi();  // Instans av en OpenAIApi-klass (förmodligen för att kommunicera med GPT-3).
 
-        private List<ChatMessage> messages = new List<ChatMessage>();
-        private string prompt = "Act as a random stranger in a chat room and reply to the questions. Don't break character. Don't ever mention that you are an AI model.";
+        private List<ChatMessage> messages = new List<ChatMessage>();  // Lista över meddelanden i chatten.
+        // En prompt som anger en bakgrundshistoria för GPT-3:s svar.
+        private string prompt = "In 2105, where magic meets circuits, ... avoid revealing your true nature.";
 
+        // Metod som körs när objektet startas i Unity.
         private void Start()
         {
+            // Lägg till en lyssnare till knappens klickhändelse.
             button.onClick.AddListener(SendReply);
         }
 
+        // Funktion för att lägga till ett nytt meddelande i chattfönstret.
         private void AppendMessage(ChatMessage message)
         {
-            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
-
-            var item = Instantiate(message.Role == "user" ? sent : received, scroll.content);
-            item.GetChild(0).GetChild(0).GetComponent<Text>().text = message.Content;
-            item.anchoredPosition = new Vector2(0, -height);
-            LayoutRebuilder.ForceRebuildLayoutImmediate(item);
-            height += item.sizeDelta.y;
-            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
-            scroll.verticalNormalizedPosition = 0;
+            // Kod för att hantera utseendet och lägga till meddelandet i chatten.
         }
 
+        // Asynkron funktion för att skicka ett svar via GPT-3 API.
         private async void SendReply()
         {
+            // Skapa ett nytt meddelandeobjekt från användarens inmatning.
             var newMessage = new ChatMessage()
             {
                 Role = "user",
                 Content = inputField.text
             };
             
-            AppendMessage(newMessage);
+            // Lägg till meddelandet i chattfönstret.
+            AppendMessage(newMessage); 
 
+            // Om det är det första meddelandet, lägg till prompten före användarens inmatning.
             if (messages.Count == 0) newMessage.Content = prompt + "\n" + inputField.text; 
             
+            // Lägg till meddelandet i meddelandelistan.
             messages.Add(newMessage);
             
+            // Inaktivera knappen och inmatningsfältet medan svar väntar.
             button.enabled = false;
             inputField.text = "";
             inputField.enabled = false;
             
-            // Complete the instruction
+            // Få svar från GPT-3 API.
             var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
             {
                 Model = "gpt-3.5-turbo-0613",
                 Messages = messages
             });
 
+            // Om det finns ett svar, lägg till det i chattfönstret.
             if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
             {
-                var message = completionResponse.Choices[0].Message;
-                message.Content = message.Content.Trim();
-                
-                messages.Add(message);
-                AppendMessage(message);
+                // Kod för att hantera svaret och lägga till det i chatten.
             }
             else
             {
+                // Om inget svar genererades, logga en varning.
                 Debug.LogWarning("No text was generated from this prompt.");
             }
 
+            // Återaktivera knappen och inmatningsfältet.
             button.enabled = true;
             inputField.enabled = true;
         }
